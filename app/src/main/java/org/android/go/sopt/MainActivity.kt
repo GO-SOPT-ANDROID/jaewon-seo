17fa7a7497d2307c75820e68285b8b53facb7e06
librarywon <librarywon@naver.com>
2023-04-08 16:56:46 +0900

    help

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -15,53 +15,12 @@ import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var id:String =""
-    private var pw:String =""
-    private var name:String =""
-    private var specialty:String =""
 
     lateinit var binding : ActivityMainBinding
-    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setResultSignUp()
-        binding.buttonMainLogin.setOnClickListener {
-                if(binding.edittextMainId.text.toString() == id && binding.edittextMainPw.text.toString()==pw) {
-                    Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-
-                    val intent = Intent(this, SelfIntroduceActivity::class.java)
-                    intent.putExtra("specialty",specialty)
-                    intent.putExtra("name",name)
-                    startActivity(intent)
-                }
-                else{
-                    Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
-                }
-        }
-        binding.buttonMainSignup.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            resultLauncher.launch(intent)
-        }
 
     }
-    private fun setResultSignUp(){
-        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if (result.resultCode == Activity.RESULT_OK){
-                id = result.data?.getStringExtra("id") ?: ""
-                pw = result.data?.getStringExtra("password")?:""
-                name = result.data?.getStringExtra("name")?:""
-                specialty = result.data?.getStringExtra("specialty")?:""
-            }
-        }
-    }
-    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        val imm: InputMethodManager =
-            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-        return super.dispatchTouchEvent(ev)
-    }
-
-
 }
---
+++ app/src/main/java/org/android/go/sopt/home/HomeFragment.kt
@@ -0,0 +1,44 @@
+package org.android.go.sopt.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.android.go.sopt.R
+import org.android.go.sopt.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "앗 ! _binding이 null이다 !" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? { // 이제 반환하는 View가 Null일 수 없기 때문에, ?를 지워주셔도 됩니다.
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 대부분의 로직은 여기에 구현합니다.
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
