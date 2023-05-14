c34431c2f3c24c6d742be82627b92ff405d66e66
librarywon <librarywon@naver.com>
2023-05-14 19:52:38 +0900

    refactor : 쿼리 요청함수 가독성 올리기 위해서 getFollwerList 함수 수정

--- app/src/main/java/org/android/go/sopt/data/remote/service/FollowerService.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/service/FollowerService.kt
@@ -3,8 +3,11 @@ package org.android.go.sopt.data.remote.service
 import org.android.go.sopt.data.remote.dto.ResponseFollowerDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FollowerService {
-    @GET("/api/users?page=1")
-    fun getFollowerList(): Call<ResponseFollowerDto>
+    @GET("/api/users")
+    fun getFollowerList(
+        @Query("page") num: Int = 2
+    ): Call<ResponseFollowerDto>
 }
