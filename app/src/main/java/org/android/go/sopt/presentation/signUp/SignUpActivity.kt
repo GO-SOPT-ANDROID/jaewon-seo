558c6eb836dc3f875f37402d5561683ae60e2183
librarywon <librarywon@naver.com>
2023-05-22 01:42:09 +0900

    feat : 양방향 데이터 통신으로 데이터 유효성 판단 구현, CommonTextWatcher 대체 삭제

--- app/src/main/java/org/android/go/sopt/presentation/signUp/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signUp/SignUpActivity.kt
@@ -9,106 +9,57 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.databinding.ActivitySignUpBinding
-import org.android.go.sopt.presentation.sginIn.SignInActivity
 
 class SignUpActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySignUpBinding
-
-    // LiveData가 저장되어 있는 ViewModel
     private val viewModel by viewModels<SignUpViewModel>()
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSignUpBtnClickListener()
-        editOnTextChangeListener()
 
-        //signUnResult 관찰자 설정 통신 성공했을 때 변화 일어남
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        // Observe signUpResult LiveData
         viewModel.signUpResult.observe(this) { signUpResult ->
             changeActivity()
-            makeToastMessage(
-                signUpResult.message
-            )
+            makeToastMessage(signUpResult.message)
         }
-        viewModel.errorResult.observe(this) {errorResult ->
+
+        viewModel.errorResult.observe(this) { errorResult ->
             makeSnackbarMessage(errorResult.message)
         }
-    }
 
-    private fun btnSetEnabled() { //입력된 텍스트 조건을 확인하여 회원 가입버튼 활성화
-        with(binding) {
-            if (canUserSignIn()) buttonSignupComplete.setEnabled(true)
-            else buttonSignupComplete.setEnabled(false)
-        }
-    }
-
-    private fun editOnTextChangeListener() { //텍스트 변경을 감지하여 btnSetEnabled함수 호출
-        with(binding) {
-            edittextSignupId.addTextChangedListener(
-                CommonTextWatcher(
-                    onChanged = { source, _, _, _ -> btnSetEnabled() }
-                )
-            )
-            edittextSignupPw.addTextChangedListener(
-                CommonTextWatcher(
-                    onChanged = { source, _, _, _ -> btnSetEnabled() }
-                )
-            )
-            edittextSignupName.addTextChangedListener(
-                CommonTextWatcher(
-                    onChanged = { source, _, _, _ -> btnSetEnabled() }
-                )
-            )
-            edittextSignupSpecialty.addTextChangedListener(
-                CommonTextWatcher(
-                    onChanged = { source, _, _, _ -> btnSetEnabled() }
-                )
-            )
-        }
-    }
-
-    private fun changeActivity() {
-        // 회원가입 성공시 화면 전환
-        val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
-        with(binding) {//바로 id,pw가 입력될 수 있게 값 전달
-            intent.putExtra("id", edittextSignupId.text.toString()) // id 값 전달
-            intent.putExtra("password", edittextSignupPw.text.toString()) // pw 값 전달
-        }
-        setResult(RESULT_OK, intent)
+        setSignUpBtnClickListener()
     }
 
     private fun setSignUpBtnClickListener() {
-        with(binding)
-        {
+        with(binding) {
             buttonSignupComplete.setOnClickListener {
-                viewModel.signUp(
-                    edittextSignupId.text.toString(),
-                    edittextSignupPw.text.toString(),
-                    edittextSignupName.text.toString(),
-                    edittextSignupSpecialty.text.toString()
-                )
+              viewModel?.signUp()
             }
         }
     }
 
+    private fun changeActivity() {
+        val intent = Intent().apply {
+            putExtra("id", viewModel.id.value) // id 값 전달
+            putExtra("password", viewModel.pw.value) // pw 값 전달
+        }
+        setResult(RESULT_OK, intent)
+        finish() // close this activity and go back
+    }
+
+
     private fun makeSnackbarMessage(string: String) {
-        Snackbar.make(
-            binding.root, string, Snackbar.LENGTH_SHORT
-        ).show()
+        Snackbar.make(binding.root, string, Snackbar.LENGTH_SHORT).show()
     }
 
     private fun makeToastMessage(string: String) {
-        Toast.makeText(
-            this, string, Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    private fun canUserSignIn(): Boolean {
-        return binding.edittextSignupId.text.length in 6..10
-                && binding.edittextSignupPw.text.length in 8..12
-                && binding.edittextSignupName.text.isNotBlank()
-                && binding.edittextSignupSpecialty.text.isNotBlank()
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
--- app/src/main/java/org/android/go/sopt/presentation/signUp/SignUpViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signUp/SignUpViewModel.kt
@@ -1,6 +1,7 @@
 package org.android.go.sopt.presentation.signUp
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.android.go.sopt.data.remote.ServicePool.signUpService
@@ -16,13 +17,43 @@ class SignUpViewModel : ViewModel() {
     private val _errorResult: MutableLiveData<ResponseSignUpDto> = MutableLiveData()
     val errorResult: LiveData<ResponseSignUpDto> = _errorResult
 
-    fun signUp(id : String,pw : String,name : String, specialty :String) {
+    val id = MutableLiveData<String>()
+    val pw = MutableLiveData<String>()
+    val name = MutableLiveData<String>()
+    val specialty = MutableLiveData<String>()
+
+    val isIdValid = MediatorLiveData<Boolean>().apply {
+        addSource(id) { value = validateId(it) }
+    }
+
+    val isPwValid = MediatorLiveData<Boolean>().apply {
+        addSource(pw) { value = validatePw(it) }
+    }
+
+    val isFormValid = MediatorLiveData<Boolean>().apply {
+        addSource(isIdValid) { value = checkFormValid() }
+        addSource(isPwValid) { value = checkFormValid() }
+    }
+
+    private fun validateId(id: String?): Boolean {
+        return id != null && id.matches(Regex("[a-zA-Z0-9]{6,10}"))
+    }
+
+    private fun validatePw(pw: String?): Boolean {
+        return pw != null && pw.matches(Regex("[a-zA-Z0-9!@#\$%^&*()]{8,12}"))
+    }
+
+    private fun checkFormValid(): Boolean {
+        return isIdValid.value == true && isPwValid.value == true && !name.value.isNullOrBlank() && !specialty.value.isNullOrBlank()
+    }
+
+    fun signUp() {
         signUpService.signIUp(
             RequestSignUpDto(
-                id,
-                pw,
-                name,
-                specialty
+                id.value ?: "",
+                pw.value ?: "",
+                name.value ?: "",
+                specialty.value ?: ""
             )
         ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
             override fun onResponse(
@@ -35,6 +66,7 @@ class SignUpViewModel : ViewModel() {
                     _errorResult.value = response.body()
                 }
             }
+
             override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
             }
         })
